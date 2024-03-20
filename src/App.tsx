f1cfb7ad1884678703f91195da3ce93b6672db24
Yevhen <onufrii.socnet@gmail.com>
2024-03-20 15:31:26 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material'
+import './App.css'
+
+import { FC } from 'react'
+
+const App: FC = () => {
+  return <Typography sx={{ fontWeight: 800 }}>App</Typography>
+}
+
+export default App
